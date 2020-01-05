2248148a7c96dffeb239f40b3702b740005bba5c
Mia <tamiapaschall@gmail.com>
2020-01-05 20:44:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog 
   
-  def=(name)
+  def name=(name)
     @name = name
   end
   
@@ -8,7 +8,7 @@ class Dog
     @name
   end
   
-  def=(breed)
+  def breed=(breed)
     @breed = breed
   end
   
